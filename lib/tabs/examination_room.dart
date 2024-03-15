f71c46d47dc2538e6ef5ca1e17c88a0332ed1d51
kimshinil79 <alienpro@naver.com>
2024-03-16 06:06:45 +0900

    tab으로 분리

---
+++ lib/tabs/examination_room.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ExaminationRoom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 검사실 탭의 내용
+    return Center(
+      child: Text('검사실 내용'),
+    );
+  }
+}
